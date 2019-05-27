942d843bd51cb724b99f154aa8c1d8df830c6f8f
Tabbs18 <tabithamclaughlin500@yahoo.com>
2019-05-27 13:34:19 -0400

    created the schema and seeds file and ran them in mysql. Also finished the connection.js file

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('cheese burger', "true");
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('bacon burger', "true");
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('chilli burger', "true");
