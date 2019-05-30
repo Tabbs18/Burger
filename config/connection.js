bcdcdc5fe0136d35bb8a29bed47103e2867d1488
Tabbs18 <tabithamclaughlin500@yahoo.com>
2019-05-29 23:44:18 -0400

    inserted code for heroku upload

--- config/connection.js
+++ config/connection.js
@@ -8,6 +8,15 @@ var connection = mysql.createConnection({
   database: "burgers_db"
 });
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'burgers_db'
+    });
+};
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
